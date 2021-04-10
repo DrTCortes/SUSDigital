15964d6f65b1e6da6ac13dd12a582e975f2a9bb9
Thiago Cortes <thiagocortes@outlook.com.br>
2021-04-10 13:51:50 -0300

    Projeto SUS Digital

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
-import * as React from 'react';
-import { View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import ListFuncionarios from './components/Funcionarios/ListFuncionarios'
-import HomeScreen from './components/HomeScreen'
-import ListPacientes from './components/Pacientes/ListPacientes'
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native'
 
-const opt = {
-    headerLeft: () => (
-      <TouchableOpacity onPress={navigation.openDrawer}>
-        <Text style={{margin: 100}}>Esquerdo</Text>
-      </TouchableOpacity>
-    )
-  };
-
-const Drawer = createDrawerNavigator();
 
 export default function App() {
-  
-  return (  
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen options={opt} name="Home" component={HomeScreen} />
-        <Drawer.Screen options={opt} name="Funcionarios" component={ListFuncionarios} />
-        <Drawer.Screen options={opt} name="Pacientes" component={ListPacientes} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-    
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txtG}>Projeto SUSDigital</Text>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'top',
+  },
+});
