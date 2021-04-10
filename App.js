5d8681a1e4577f91a469cfe83fb657c01cc60587
Thiago Cortes <thiagocortes@outlook.com.br>
2021-04-10 15:33:11 -0300

    Adicionando Bibliotecas

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
 import * as React from 'react';
 import { View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
 import ListFuncionarios from './components/Funcionarios/ListFuncionarios'
 import HomeScreen from './components/HomeScreen'
 import ListPacientes from './components/Pacientes/ListPacientes'
 
-const opt = {
-    headerLeft: () => (
-      <TouchableOpacity onPress={navigation.openDrawer}>
-        <Text style={{margin: 100}}>Esquerdo</Text>
-      </TouchableOpacity>
-    )
-  };
+// const opt = {
+//     headerLeft: () => (
+//       <TouchableOpacity onPress={navigation.openDrawer}>
+//         <Text style={{margin: 100}}>Esquerdo</Text>
+//       </TouchableOpacity>
+//     )
+//   };
 
-const Drawer = createDrawerNavigator();
+  const Stack = createStackNavigator();
 
 export default function App() {
   
   return (  
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen options={opt} name="Home" component={HomeScreen} />
-        <Drawer.Screen options={opt} name="Funcionarios" component={ListFuncionarios} />
-        <Drawer.Screen options={opt} name="Pacientes" component={ListPacientes} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen  name="Home" component={HomeScreen} />
+        <Stack.Screen  name="Funcionarios" component={ListFuncionarios} />
+        <Stack.Screen  name="Pacientes" component={ListPacientes} />
+      </Stack.Navigator>
     </NavigationContainer>
     
   );
