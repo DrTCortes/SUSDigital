cf6ccc3e4b45ea629add3e9c2ef12b8b63d13698
Thiago Cortes <thiagocortes@outlook.com.br>
2021-04-17 18:49:26 -0300

    Trabalhando na Intergração de Telas

--- components/Dev/DevArea.js
+++ components/Dev/DevArea.js
@@ -2,11 +2,12 @@ import React, {useContext} from 'react';
 import { View, FlatList, Alert } from 'react-native';
 
 import { ListItem, Avatar, Button, Icon} from 'react-native-elements';
+import Styles from '../styles';
   
 export default props => {
 return(
-  <View>
-    <Button title={"Funcionarios"} onPress={()=>{ props.navigation.navigate("Funcionarios")}}/>
+  <View style={[Styles.form, {margin: 1}]}>
+    <Button type='clear' title={"Funcionarios"} onPress={()=>{ props.navigation.navigate("Funcionarios")}}/>
     <Button title={"Médicos"} onPress={()=>{ props.navigation.navigate("Medicos")}}/>
     <Button title={"Especialidades"} onPress={()=>{ props.navigation.navigate("Especialidades")}}/>
     <Button title={"Postos"} onPress={()=>{ props.navigation.navigate("Postos")}}/>
