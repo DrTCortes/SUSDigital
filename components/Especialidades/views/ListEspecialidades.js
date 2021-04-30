2496586129676f5cab3c972ccce9bd409d536d9d
Tarik Lopes <tarikhero@gmail.com>
2021-04-29 23:30:43 -0300

    Melhorias em especialidades

--- components/Especialidades/views/ListEspecialidades.js
+++ components/Especialidades/views/ListEspecialidades.js
@@ -1,43 +1,25 @@
 import React, { useContext } from 'react'
-import { View, FlatList, Alert } from 'react-native'
+import { View, FlatList, Alert, Image, TouchableOpacity } from 'react-native'
 import { ListItem, Button, Icon, Avatar } from 'react-native-elements'
-import Context from '../../context/AppContext'
+import AppContext from '../../context/AppContext'
+import Styles from '../../styles'
 
 export default props => {
 
-    const { state, dispatch } = useContext(Context)
+    const { state, dispatch } = useContext(AppContext)
 
-    //Função para exibir um modal de confirmação de deletar a especialidade
-    function confirmEspecDeletion(espec) {
-        Alert.alert('Excluir Especialidade?', 'Deseja excluir o Especialidade?', [
-            { text: 'Sim',
-                onPress() {
-                    dispatch({
-                        type: 'deleteEspec',
-                        payload: espec,
-                    })}
-            },
-            {
-                text: 'Não'
-            }
-        ])}
-
-    //Função onde ficam os botões de editar e excluir especialidade
     function getActions(espec) {
         return (
             <>
-                {/* Botão para editar */}
                 <Button
                     onPress={() => props.navigation.navigate('FormEspec', espec)}
                     type="clear"
-                    icon={<Icon name="edit" size={25} color="#188dbb" />}
+                    icon={<Icon name="edit" size={25} color="#FFF" />}
                 />
-
-                {/* Botão para excluir */}
                 <Button
                     onPress={() => dispatch({type: 'deleteEspec', payload: espec})}
                     type="clear"
-                    icon={<Icon name="delete" size={25} color="#188dbb" />}
+                    icon={<Icon name="delete" size={25} color="#FFF" />}
                 />
             </>
         )
@@ -45,19 +27,19 @@ export default props => {
 
     function getEspecItem({ item: espec }) {
         return (
-            <ListItem key={espec.id} bottomDivider rightElement={getActions(espec)}
+            <TouchableOpacity style={[Styles.contentBox, {backgroundColor: '#188dbb'}]} key={espec.id} bottomDivider rightElement={getActions(espec)}
                 onPress={() => props.navigation.navigate('InfoEspec', espec)}>
-                    <Avatar tittle={espec.name} rounded source={espec.avatarUrl && { uri: espec.avatarUrl }}/>
+                    <Image style={Styles.imageIcon} rounded source={espec.avatarUrl && { uri: espec.avatarUrl }}/>
                     <ListItem.Content>
-                        <ListItem.Title>{espec.name}</ListItem.Title>
-                        <ListItem.Subtitle>{espec.descricao}</ListItem.Subtitle>
+                        <ListItem.Title style={{color: '#FFF'}}>{espec.name}</ListItem.Title>
+                        <ListItem.Subtitle style={{color: '#c9c9c9'}}>{espec.email}</ListItem.Subtitle>
                     </ListItem.Content>
                         <View style={{flexDirection:'row'}}>{getActions(espec)}</View>
-            </ListItem>
+            </TouchableOpacity>
         )}
 
     return (
-        <View>
+        <View style={[Styles.container, {alignItems: 'center'}]}>
             <FlatList
                 keyExtractor={espec => espec.id.toString()}
                 data={state.especs}
